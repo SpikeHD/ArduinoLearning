b0dba40da50dd5dcb4e821bafdd45a35a3089504
spikegd <spikegdofficial@gmail.com>
2020-08-07 21:45:22 -0700

    Uber-basic snake AI

--- snake/snake.ino
+++ snake/snake.ino
@@ -1,8 +1,10 @@
 #include <LiquidCrystal_I2C.h>
 
 int btn_pin = 2;
+int x_lim = 20, y_lim = 4;
+int curFoodX, curFoodY, curAiX = x_lim, curAiY = y_lim;
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+LiquidCrystal_I2C lcd(0x27, x_lim, y_lim);
 
 void setup() {
   pinMode(btn_pin, INPUT);
@@ -13,15 +15,43 @@ void setup() {
 
 void loop() {
   int btnState = digitalRead(btn_pin);
-  
+
   lcd.clear();
   lcd.setCursor(0,0);
   
   if(btnState == HIGH) {
-    lcd.print("On!");
-  } else {
-    lcd.print("Off!");
+    foodSpawn();
   }
 
-  delay(100);
+  ai();
+
+  delay(500);
+}
+
+void foodSpawn() {
+  curFoodX = random(x_lim);
+  curFoodY = random(y_lim);
+}
+
+void ai() {
+  int x_dir = curFoodX - curAiX,
+      y_dir = curFoodY - curAiY;
+
+  if(x_dir < 0) {
+    curAiX--;
+  } else if (x_dir > 0) {
+    curAiX++;
+  } else if (y_dir < 0) {
+    curAiY--;
+  } else if (y_dir > 0) {
+    curAiY++;
+  } else {
+    foodSpawn();
+  }
+  
+  lcd.setCursor(curAiX, curAiY);
+  lcd.print("S");
+  
+  lcd.setCursor(curFoodX, curFoodY);
+  lcd.print("0");
 }
